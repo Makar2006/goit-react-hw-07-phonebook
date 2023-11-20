fbe7fdd896f4176d74f7d9c3e699a849a540bedb
Makar2006 <makar.diakiv@gmail.com>
2023-11-20 18:48:13 +0100

    first-init

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: {
+    contacts: [],
+    filter: '',
+  },
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts = [...state.contacts, action.payload];
+    },
+    removeContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        cont => cont.id !== action.payload.id
+      );
+    },
+    setFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
+});
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['contacts'],
+};
+export const persistedReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
+
+export const { addContact, removeContact, setFilter } = contactsSlice.actions;
